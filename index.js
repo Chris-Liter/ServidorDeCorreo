f41e48486c810de3f243a58461423f50a771732e
Chris-Liter <136271203+Chris-Liter@users.noreply.github.com>
2024-06-27 22:38:42 -0500

    first commit

--- index.js
+++ index.js
@@ -10,19 +10,17 @@ app.use(cors());
 app.post('/api/persona', (req, res) => {
   const persona = req.body;
 
-  // Configura el transporte de nodemailer
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'jorgelituma094@gmail.com', // Cambia esto con tu correo
-      pass: 'pure mgej fvnc acwh'        // Cambia esto con tu contraseña
+      user: 'jorgelituma094@gmail.com', 
+      pass: 'pure mgej fvnc acwh'       
     }
   });
 
-  // Configura el correo
   let mailOptions = {
-    from: 'jorgelituma094@gmail.com', // Cambia esto con tu correo
-    to: 'jorgelituma096@gmail.com', // Cambia esto con el correo del destinatario
+    from: 'jorgelituma094@gmail.com', 
+    to: 'jorgelituma096@gmail.com', 
     subject: 'Información de Persona',
     text: `Cédula: ${persona.cedula}\nNombre: ${persona.nombre}\nApellido: ${persona.apellido}\nEdad: ${persona.edad}`
   };
